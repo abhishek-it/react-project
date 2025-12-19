ddff8e373f88ad9ef02ce6f83692c6686d8670dd
abhishek-it <abhishekgiri6729@gmail.com>
2025-12-20 02:07:05 +0530

    20 DEC

--- my-react-app/src/components/Postlist.jsx
+++ my-react-app/src/components/Postlist.jsx
@@ -2,9 +2,17 @@ import classes from "./PostLists.module.css"
 import Post from "./Post";
 import NewPost from "./NewPost";
 import { useState } from "react";
+import Modal from "./Modal";
 function Postlist() {
+    //here we will declare a state to define visible and not
+    const [isVisible , setVisible] = useState(true);
     const [text , setText] = useState("");
     const [name , setName] = useState("");
+
+    //define a function to show/hide the componnet
+    function setVisibility(){
+        setVisible(false);
+    }
     function handlText(event){
         setText(event.target.value);
     }
@@ -13,11 +21,17 @@ function Postlist() {
     }
     return (
         <>
+        {isVisible ?
+        <Modal onClose = {setVisibility}>
         <NewPost ontext = {handlText} onname = {handleName} />
+        </Modal> :false}
         <ul className = {classes.posts}>
         <Post author = {name} body = {text} />
+        
         <Post author ={name} body ={text}/>
+    
         </ul>
+        
         </>
     )
 }
